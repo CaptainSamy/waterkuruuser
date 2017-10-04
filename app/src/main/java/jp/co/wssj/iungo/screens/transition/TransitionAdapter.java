18d3334f35008751fb0616ed28595145b8064d00
Huu Ta <tanh@watermelon.vn>
2017-10-04 16:48:46 +0700

    transition view

---
+++ app/src/main/java/jp/co/wssj/iungo/screens/transition/TransitionAdapter.java
@@ -0,0 +1,9 @@
+package jp.co.wssj.iungo.screens.transition;
+
+/**
+ * Created by Nguyen Huu Ta on 2/10/2017.
+ */
+
+public class TransitionAdapter {
+
+}
---
+++ app/src/main/java/jp/co/wssj/iungo/widget/CustomViewPager.java
@@ -0,0 +1,9 @@
+package jp.co.wssj.iungo.widget;
+
+/**
+ * Created by Nguyen Huu Ta on 2/10/2017.
+ */
+
+public class CustomViewPager {
+
+}
