71f229455218359ae94081e504a3b8d3d5554824
NguyenHuuTa <nguyenhuuta456@gmail.com>
2019-08-01 09:32:02 +0700

    fix bug log null crash

--- app/src/main/java/wssj/co/jp/olioa/utils/Logger.java
+++ app/src/main/java/wssj/co/jp/olioa/utils/Logger.java
@@ -13,30 +13,35 @@ public final class Logger {
     private static final int LOG_LEVEL = BuildConfig.DEBUG ? Log.VERBOSE : Log.INFO;
 
     public static void v(String tag, String message) {
+        if (message == null)return;
         if (Log.VERBOSE >= LOG_LEVEL) {
             Log.v(tag, message);
         }
     }
 
     public static void d(String tag, String message) {
+        if (message == null)return;
         if (Log.DEBUG >= LOG_LEVEL) {
             Log.d(tag, message);
         }
     }
 
     public static void i(String tag, String message) {
+        if (message == null)return;
         if (Log.INFO >= LOG_LEVEL) {
             Log.i(tag, message);
         }
     }
 
     public static void w(String tag, String message) {
+        if (message == null)return;
         if (Log.WARN >= LOG_LEVEL) {
             Log.w(tag, message);
         }
     }
 
     public static void e(String tag, String message) {
+        if (message == null)return;
         if (Log.ERROR >= LOG_LEVEL) {
             Log.e(tag, message);
         }
